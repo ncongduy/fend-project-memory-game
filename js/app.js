c0b5a6fb003b516c2f2611877a533b191f48d090
Cong Duy <ncongduy@gmail.com>
2020-06-27 15:29:21 +0300

    refactor: refactoring production code clearly

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
-// Create a list that holds all of cards
+//Create a list that holds all of cards
 let listOfCards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
 
-// Declare variable
+//Declare variable
 const deck = document.querySelector('.deck');
 const fragmentCard = document.createDocumentFragment();
 let cardsOpen = [];
 
-// Shuffle the list of cards; Shuffle function from http://stackoverflow.com/a/2450976
+//Shuffle the list of cards; Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -35,7 +35,7 @@ function addCardsToPage() {
     deck.appendChild(fragmentCard);
 }
 
-//Set up two cards when match
+//Open two cards when match
 function twoCardsMatch() {
     if (cardsOpen[0] === cardsOpen[1]) {
         const match = deck.getElementsByClassName(cardsOpen[0]);
@@ -45,16 +45,11 @@ function twoCardsMatch() {
     };
 }
 
-//Set up two cards when NOT match
+//Close two cards when NOT match
 function twoCardsNotMatch() {
     if (cardsOpen[0] !== cardsOpen[1]) {
-        console.log(cardsOpen);
-        console.log(cardsOpen[0]);
-        console.log(cardsOpen[1]);
         const diff1 = deck.getElementsByClassName(cardsOpen[0]);
-        console.log(diff1);
         const diff2 = deck.getElementsByClassName(cardsOpen[1]);
-        console.log(diff2);
         diff1[0].parentElement.className = 'card';
         diff1[1].parentElement.className = 'card';
         diff2[0].parentElement.className = 'card';
@@ -63,26 +58,27 @@ function twoCardsNotMatch() {
     }
 }
 
-// set up the event listener for a card. If a card is clicked
-deck.addEventListener('click', function (evt) {
-    if (evt.target.nodeName === 'LI') {
-        evt.target.className = 'card open show';
-        const icon = evt.target.querySelector('i').className;
-        cardsOpen.push(icon);
-        console.log(cardsOpen);
-        if (cardsOpen.length === 2) {
-            twoCardsMatch();
-            setTimeout(twoCardsNotMatch, 800);
+//Set up the event listener for a card. If a card is clicked
+function addCardsListener() {
+    deck.addEventListener('click', function (evt) {
+        if (evt.target.nodeName === 'LI') {
+            evt.target.className = 'card open show';
+            const icon = evt.target.querySelector('i').className;
+            cardsOpen.push(icon);
+            if (cardsOpen.length === 2) {
+                twoCardsMatch();
+                setTimeout(twoCardsNotMatch, 500);
+            }
         }
-    }
-});
-
-
+    });
+}
 //   - if the list already has another card, check to see if the two cards match
-//     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-//     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+//     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) - Done
+//     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) - Done
 //     + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
 //     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 
+//Run code
 shuffle(listOfCards);
 addCardsToPage();
+addCardsListener();
