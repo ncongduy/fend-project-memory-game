5f47d4cd4b7bca75669d61e1831e9444a1f7d78d
Cong Duy <ncongduy@gmail.com>
2020-06-30 12:52:41 +0300

    feat: add count moves

--- js/app.js
+++ js/app.js
@@ -35,13 +35,19 @@ function addCardsToPage() {
     deck.appendChild(fragmentCard);
 }
 
+//Delete cards 
+function deleteCards() {
+    cardsOpen = [];
+}
+
 //Open two cards when match
 function twoCardsMatch() {
     if (cardsOpen[0] === cardsOpen[1]) {
         const match = deck.getElementsByClassName(cardsOpen[0]);
         match[0].parentElement.className = 'card match';
         match[1].parentElement.className = 'card match';
-        cardsOpen = [];
+        countMove();
+        setTimeout(deleteCards, 500);
     };
 }
 
@@ -54,7 +60,8 @@ function twoCardsNotMatch() {
         diff1[1].parentElement.className = 'card';
         diff2[0].parentElement.className = 'card';
         diff2[1].parentElement.className = 'card';
-        cardsOpen = [];
+        countMove();
+        setTimeout(deleteCards, 500);
     }
 }
 
@@ -72,6 +79,18 @@ function addCardsListener() {
         }
     });
 }
+
+function countMove() {
+    if (cardsOpen.length === 2) {
+        let moves = document.querySelector('.moves');
+        let stringMoves = moves.textContent;
+        let numberMoves = parseInt(stringMoves, 10);
+        numberMoves += 1;
+        moves.textContent = numberMoves.toString();
+    }
+}
+
+countMove();
 //   - if the list already has another card, check to see if the two cards match
 //     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) - Done
 //     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) - Done
