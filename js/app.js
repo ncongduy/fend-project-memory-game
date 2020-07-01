a46332d2218eddb6ec80676c66ea6311fc8246c5
Cong Duy <ncongduy@gmail.com>
2020-07-01 18:21:38 +0300

    feat: add timer when click first card

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ let listOfCards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa f
 const deck = document.querySelector('.deck');
 const fragmentCard = document.createDocumentFragment();
 let cardsOpen = [];
+let interval = null;
 
 //Shuffle the list of cards; Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -46,7 +47,7 @@ function twoCardsMatch() {
         const match = deck.getElementsByClassName(cardsOpen[0]);
         match[0].parentElement.className = 'card match';
         match[1].parentElement.className = 'card match';
-        setTimeout(deleteCards, 500);
+        deleteCards();
     };
 }
 
@@ -59,8 +60,56 @@ function twoCardsNotMatch() {
         diff1[1].parentElement.className = 'card';
         diff2[0].parentElement.className = 'card';
         diff2[1].parentElement.className = 'card';
-        setTimeout(deleteCards, 500);
+        deleteCards();
     }
+}
+
+//Set up count movement
+function countMove() {
+    const moves = document.querySelector('.moves');
+    const stringMoves = moves.textContent;
+    let numberMoves = parseInt(stringMoves, 10);
+    numberMoves += 1;
+    moves.textContent = numberMoves.toString();
+}
+
+//Set up two number letter which is lower than 10: 00, 01, 02,...
+function addZero(value) {
+    let time = value.toString();
+    if (time.length < 2) {
+        return "0" + time;
+    } else {
+        return time;
+    }
+}
+
+//Set up Timer
+function timer() {
+    const startTime = Date.now();
+    interval = setInterval(function () {
+        let deltaTime = Date.now() - startTime;
+        let minutes = Math.floor(deltaTime / (60 * 1000));
+        let seconds = Math.floor(deltaTime % (60 * 1000) / 1000);
+        let minutesLabel = document.querySelector('#minutes');
+        let secondsLabel = document.querySelector('#seconds');
+        minutesLabel.textContent = addZero(minutes);
+        secondsLabel.textContent = addZero(seconds);
+    }, 1000);
+}
+
+//Run timer when click first card
+function click(evt){
+    if (evt.target.nodeName === 'LI') {
+        timer();
+        deck.removeEventListener('click', click);
+    }
+}
+
+deck.addEventListener('click', click);
+
+//Stop Timer when win game
+function clearTimer() {
+    clearInterval(interval);
 }
 
 //Set up the event listener for a card. If a card is clicked
@@ -79,20 +128,12 @@ function addCardsListener() {
     });
 }
 
-//Set up count movement
-function countMove() {
-        const moves = document.querySelector('.moves');
-        const stringMoves = moves.textContent;
-        let numberMoves = parseInt(stringMoves, 10);
-        numberMoves += 1;
-        moves.textContent = numberMoves.toString();
-}
-
 //   - if the list already has another card, check to see if the two cards match
 //     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) - Done
 //     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) - Done
 //     + increment the move counter and display it on the page (put this functionality in another function that you call from this one) - Done
-//     + add count time
+//     + add count time - Done
+//     + fix mistake double click one card
 //     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 
 //Run code
