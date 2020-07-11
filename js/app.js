9548d71df5531124042009d79879a602ade780af
Cong Duy <ncongduy@gmail.com>
2020-07-11 13:51:14 +0300

    refactor: refactoring all code in js file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-//Create a list that holds all of cards
-let listOfCards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
-
 //Declare variable
 const deck = document.querySelector('.deck');
 const fragmentCard = document.createDocumentFragment();
@@ -12,6 +9,9 @@ let cardsOpen = [];
 let interval;
 let minutesLabel = document.querySelector('#minutes');
 let secondsLabel = document.querySelector('#seconds');
+
+//Create a list that holds all of cards
+let listOfCards = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
 
 //Shuffle the list of cards; Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -42,9 +42,27 @@ function addCardsToPage() {
     deck.appendChild(fragmentCard);
 }
 
-//Delete cards 
-function deleteCards() {
-    cardsOpen = [];
+//Set up the event listener for a card. If a card is clicked
+function addCardsListener() {
+    deck.addEventListener('click', function (evt) {
+        if (evt.target.nodeName === 'LI' && evt.target.className === 'card') {
+            evt.target.className = 'card open show';
+            const icon = evt.target.querySelector('i').className;
+            cardsOpen.push(icon);
+            if (cardsOpen.length === 2) {
+                countMove();
+                twoCardsMatch();
+                setTimeout(twoCardsNotMatch, 500);
+            }
+            winGame();
+        }
+    });
+}
+
+//Set up count movement
+function countMove() {
+    numberMoves += 1;
+    moves.textContent = numberMoves.toString();
 }
 
 //Open two cards when match
@@ -70,19 +88,23 @@ function twoCardsNotMatch() {
     }
 }
 
-//Set up count movement
-function countMove() {
-    numberMoves += 1;
-    moves.textContent = numberMoves.toString();
+//Delete cards 
+function deleteCards() {
+    cardsOpen = [];
 }
 
-//Set up two number letter which is lower than 10: 00, 01, 02,...
-function addZeroIntoNumber(value) {
-    let time = value.toString();
-    if (time.length < 2) {
-        return "0" + time;
-    } else {
-        return time;
+//Win game
+function winGame() {
+    const cardsGame = deck.querySelectorAll('li');
+    let cardMatch = [];
+    for (let i = 0; i < cardsGame.length; i++) {
+        if (cardsGame[i].className === 'card match') {
+            cardMatch.push(cardsGame[i].className);
+            if (cardMatch.length === cardsGame.length) {
+                StopTimer();
+                setTimeout(popUpMessage, 500);
+            }
+        }
     }
 }
 
@@ -98,6 +120,16 @@ function timer() {
     }, 1000);
 }
 
+//Set up two number letter which is lower than 10: 00, 01, 02,...
+function addZeroIntoNumber(value) {
+    let time = value.toString();
+    if (time.length < 2) {
+        return "0" + time;
+    } else {
+        return time;
+    }
+}
+
 //Run timer when click first card
 function click(evt) {
     if (evt.target.nodeName === 'LI') {
@@ -108,17 +140,32 @@ function click(evt) {
 
 deck.addEventListener('click', click);
 
-//Restart game
-function restart() {
-    const restartIcon = document.querySelector('.restart');
-    restartIcon.addEventListener('click', function () {
-        location.reload();
-    })
-}
-
 //Stop Timer when win game
 function StopTimer() {
     clearInterval(interval);
+}
+
+// Pop up message when win game
+function popUpMessage() {
+    popUpBox.innerHTML =
+        `<div class="popUpContainer">
+            <div>Congratulation! You won the game.</div>
+            <div>You used <b>${moves.textContent}</b> moves and <b>${minutesLabel.textContent}</b> minutes <b>${secondsLabel.textContent}</b> seconds</div>
+            <div class="ratingStar">${voteStart()}</div>
+            <div>Do you want to play again?</div>
+            <div class="boxAnswer">
+                <span class="answer yes">Yes</span>
+                <span class="answer no">No</span>
+            </div>
+        </div>`;
+    const yesIcon = document.querySelector('.yes');
+    const noIcon = document.querySelector('.no');
+    yesIcon.addEventListener('click', function(){
+        location.reload();
+    });
+    noIcon.addEventListener('click', function(){
+        popUpBox.innerHTML = "";
+    });    
 }
 
 // Rating star
@@ -144,70 +191,13 @@ function voteStart() {
     }
 }
 
-// Pop up message when win game
-function popUpMessage() {
-    popUpBox.innerHTML =
-        `<div class="popUpContainer">
-        <div>Congratulation! You won the game.</div>
-        <div>You used <b>${moves.textContent}</b> moves and <b>${minutesLabel.textContent}</b> minutes <b>${secondsLabel.textContent}</b> seconds</div>
-        <div class="ratingStar">${voteStart()}</div>
-        <div>Do you want to play again?</div>
-        <div class="boxAnswer">
-            <span class="answer yes">Yes</span>
-            <span class="answer no">No</span>
-        </div>
-    </div>`;
-    const yesIcon = document.querySelector('.yes');
-    const noIcon = document.querySelector('.no');
-    yesIcon.addEventListener('click', function(){
+//Restart game
+function restart() {
+    const restartIcon = document.querySelector('.restart');
+    restartIcon.addEventListener('click', function () {
         location.reload();
-    });
-    noIcon.addEventListener('click', function(){
-        popUpBox.innerHTML = "";
-    });    
+    })
 }
-
-//Win game
-function winGame() {
-    const cardsGame = deck.querySelectorAll('li');
-    let cardMatch = [];
-    for (let i = 0; i < listOfCards.length; i++) {
-        if (cardsGame[i].className === 'card match') {
-            cardMatch.push(cardsGame[i].className);
-            if (cardMatch.length === listOfCards.length) {
-                StopTimer();
-                setTimeout(popUpMessage, 500);
-            }
-        }
-    }
-}
-
-//Set up the event listener for a card. If a card is clicked
-function addCardsListener() {
-    deck.addEventListener('click', function (evt) {
-        if (evt.target.nodeName === 'LI') {
-            evt.target.className = 'card open show';
-            const icon = evt.target.querySelector('i').className;
-            cardsOpen.push(icon);
-            if (cardsOpen.length === 2) {
-                countMove();
-                twoCardsMatch();
-                setTimeout(twoCardsNotMatch, 500);
-            }
-            winGame();
-        }
-    });
-}
-
-//   - if the list already has another card, check to see if the two cards match
-//     + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one) - Done
-//     + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one) - Done
-//     + increment the move counter and display it on the page (put this functionality in another function that you call from this one) - Done
-//     + add count time - Done
-//     + add restart feature - Done
-//     + stop timer when all cards match - Done
-//     + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-//     + fix mistake double click one card
 
 //Run code
 shuffle(listOfCards);
